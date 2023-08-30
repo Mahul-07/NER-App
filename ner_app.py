d5ebcc58706a2367d20ef3e5e91a6a7723041ca7
Mahul Rana <mahul.r@idsil.loc>
2023-08-30 22:01:51 +0530

    added custom tabels and done some formating on ner output

--- ner_app.py
+++ ner_app.py
@@ -1,6 +1,7 @@
 import spacy
-import spacy_streamlit
+import pandas as pd
 import streamlit as st
+from spacy import displacy
 from streamlit_option_menu import option_menu
 
 
@@ -23,14 +24,34 @@ selected = option_menu(
 if selected =='NER':
     def main():
         raw_data = st.text_area("Enter Input Text here")
-        docx = nlp(raw_data)
         if st.button("Show Entities"):
             if raw_data == '':
                 st.warning("Sorry, Please input your data to access this functionality!!")
             else:
-                spacy_streamlit.visualize_ner(docx, labels = nlp.get_pipe('ner').labels)
-    main()
+                doc = nlp(raw_data)
+                spans = [
+                    {
+                        "start": ent.start_char,
+                        "end": ent.end_char,
+                        "label": ent.label_,
+                    }
+                    for ent in doc.ents
+                ]
+                html = displacy.render(
+                    {"text": raw_data, "ents": spans},
+                    style="ent",
+                    manual=True,
+                    page=False,
+                )
+                st.write(html, unsafe_allow_html=True)
+                st.write("")
+                st.write("")
+                data =[(ent.text, ent.label_) for ent in doc.ents]
+                df = pd.DataFrame(data,columns=['Text','Entities'])
+                st.dataframe(df,use_container_width=True)
 
+    main()
+   
 if selected =='Medical NER':
     col_dict = {}
     seven_colours = ['#e6194B', '#3cb44b', '#ffe119', '#ffd8b1', '#f58231', '#f032e6', '#42d4f4']
@@ -47,6 +68,11 @@ if selected =='Medical NER':
             doc = med7(text)
             html=spacy.displacy.render(doc, style='ent', options=options)
             st.write(html,unsafe_allow_html=True)
+            st.write("")
+            st.write("")
+            data =[(ent.text, ent.label_) for ent in doc.ents]
+            df = pd.DataFrame(data,columns=['Text','Entities'])
+            st.dataframe(df,use_container_width=True)
 
 
 
